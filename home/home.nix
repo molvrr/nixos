2b52fe7817e3f5efc48424b00ffe0b6ffc14998e
Mateus Cruz <mateuscolvr@gmail.com>
2023-07-25 00:20:09 -0300

    create hyprland module

--- home/home.nix
+++ home/home.nix
@@ -11,6 +11,7 @@ in {
     # ./modules/dunst.nix
     # ./modules/i3.nix
     ./modules/minecraft.nix
+    ./modules/hyprland.nix
     ./modules/mpv.nix
     ./modules/tmux.nix
   ];
@@ -110,100 +111,5 @@ in {
   };
 
   services.emacs.enable = true;
-
-  wayland.windowManager.hyprland = {
-    enable = true;
-    settings = {
-      monitor = ",preferred,auto,auto";
-      exec-once = "waybar & firefox & hyprpaper";
-      input = {
-        kb_layout = "us";
-        kb_variant = "intl";
-        kb_model = "";
-        kb_options = "";
-        kb_rules = "";
-        follow_mouse = 1;
-        touchpad = { natural_scroll = false; };
-        sensitivity = 0;
-      };
-
-      general = {
-        gaps_in = 5;
-        gaps_out = 10;
-        border_size = 2;
-        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-        "col.inactive_border" = "rgba(595959aa)";
-        layout = "dwindle";
-      };
-
-      decoration = {
-        rounding = 3;
-        blur = true;
-        blur_size = 7;
-        blur_passes = 3;
-        blur_new_optimizations = true;
-        drop_shadow = true;
-        shadow_range = 4;
-        shadow_render_power = 3;
-        "col.shadow" = "rgba(1a1a1aee)";
-      };
-
-      animations = { enabled = false; };
-
-      dwindle = {
-        pseudotile = true;
-        preserve_split = true;
-      };
-
-      master = { new_is_master = true; };
-
-      gestures = { workspace_swipe = false; };
-
-      "device:epic-mouse-v1" = { sensitivity = -0.5; };
-
-      "$mainMod" = "SUPER";
-
-      bind = [
-        "$mainMod, Return, exec, alacritty"
-        "$mainMod SHIFT, Q, killactive,"
-        "$mainMod, M, exit,"
-        "$mainMod, V, togglefloating,"
-        "$mainMod, R, exec, wofi --show drun"
-        "$mainMod, P, pseudo, # dwindle"
-        ", XF86AudioRaiseVolume, exec, pactl set-sink-volume @DEFAULT_SINK@ +5%"
-        ", XF86AudioLowerVolume, exec, pactl set-sink-volume @DEFAULT_SINK@ -5%"
-        "$mainMod, H, movefocus, l"
-        "$mainMod, L, movefocus, r"
-        "$mainMod, K, movefocus, u"
-        "$mainMod, J, movefocus, d"
-        "$mainMod, 1, workspace, 1"
-        "$mainMod, 2, workspace, 2"
-        "$mainMod, 3, workspace, 3"
-        "$mainMod, 4, workspace, 4"
-        "$mainMod, 5, workspace, 5"
-        "$mainMod, 6, workspace, 6"
-        "$mainMod, 7, workspace, 7"
-        "$mainMod, 8, workspace, 8"
-        "$mainMod, 9, workspace, 9"
-        "$mainMod, 0, workspace, 10"
-        "$mainMod SHIFT, 1, movetoworkspacesilent, 1"
-        "$mainMod SHIFT, 2, movetoworkspacesilent, 2"
-        "$mainMod SHIFT, 3, movetoworkspacesilent, 3"
-        "$mainMod SHIFT, 4, movetoworkspacesilent, 4"
-        "$mainMod SHIFT, 5, movetoworkspacesilent, 5"
-        "$mainMod SHIFT, 6, movetoworkspacesilent, 6"
-        "$mainMod SHIFT, 7, movetoworkspacesilent, 7"
-        "$mainMod SHIFT, 8, movetoworkspacesilent, 8"
-        "$mainMod SHIFT, 9, movetoworkspacesilent, 9"
-        "$mainMod SHIFT, 0, movetoworkspacesilent, 10"
-      ];
-
-      bindm = [
-        "$mainMod, mouse:272, movewindow"
-        "$mainMod, mouse:273, resizewindow"
-      ];
-    };
-  };
-
   services.mako.enable = true;
 }
