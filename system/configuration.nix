ce2fee2b3fe976dee7a06981553c2df3f8017a80
Mateus Cruz <mateuscolvr@gmail.com>
2023-08-12 02:49:45 -0300

    remove mitm cert

--- system/configuration.nix
+++ system/configuration.nix
@@ -138,10 +138,5 @@
     enable = true;
     extraPortals = with pkgs; [ xdg-desktop-portal-gtk xdg-desktop-portal-hyprland ];
   };
-
-  security.pki.certificateFiles = [
-    ./mitmproxy-ca-cert.pem
-  ];
-
   # programs.hyprland.enable = true;
 }
