98ffe7ca11638ae7525d1de4f7baeba0b2d63193
Mateus Cruz <mateuscolvr@gmail.com>
2023-08-14 16:50:07 -0300

    add pantheon tray

--- system/configuration.nix
+++ system/configuration.nix
@@ -40,7 +40,21 @@
   services.xserver.enable = true;
 
   services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.pantheon.enable = true;
+  services.xserver.desktopManager.pantheon = {
+    enable = true;
+    extraWingpanelIndicators = with pkgs; [
+      monitor
+      wingpanel-indicator-ayatana
+    ];
+  };
+  systemd.user.services.indicatorapp = {
+    description = "indicator-application-gtk3";
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.indicator-application-gtk3}/libexec/indicator-application/indicator-application-service";
+    };
+  };
 
   services.xserver.exportConfiguration = true;
   services.xserver = {
