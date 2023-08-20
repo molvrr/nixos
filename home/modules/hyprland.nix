ae46acb8d7f6595d72be0ab25dcbfc0fb2a9a6ff
Mateus Cruz <mateuscolvr@gmail.com>
2023-08-20 04:07:16 -0300

    disable hyprland animations

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -53,7 +53,7 @@
       };
 
       animations = {
-        enabled = true;
+        enabled = false;
         bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
         animation = [
           "windows, 1, 7, myBezier"
