63c9191dfe6c6feb44e96aa7b88e2a06a25d2cff
Mateus Cruz <mateuscolvr@gmail.com>
2023-10-19 14:46:43 -0300

    enable avahi

--- system/default.nix
+++ system/default.nix
@@ -166,4 +166,21 @@
     nvidiaPatches = true;
     xwayland.enable = true;
   };
+
+  networking.firewall.allowedTCPPorts = [ 7000 7001 7100 ];
+  networking.firewall.allowedUDPPorts = [ 5353 6000 6001 7011 ];
+
+  # To enable network-discovery
+  services.avahi = {
+    enable = true;
+    nssmdns = true;  # printing
+    openFirewall = true; # ensuring that firewall ports are open as needed
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+      userServices = true;
+      domain = true;
+    };
+  };
 }
