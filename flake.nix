f27258ce1ca8b531dd149bd9b0bb23dbd4165142
Mateus Cruz <mateuscolvr@gmail.com>
2023-11-08 13:11:53 -0300

    add neovim overlay

--- flake.nix
+++ flake.nix
@@ -3,15 +3,17 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     ssbm.url = "github:djanatyn/ssbm-nix";
+    neovim.url = "github:nix-community/neovim-nightly-overlay";
+    neovim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, home-manager, nixpkgs, ssbm }:
+  outputs = { self, home-manager, nixpkgs, ssbm, neovim }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
-        overlays = [ ssbm.overlay ];
+        overlays = [ ssbm.overlay neovim.overlay ];
       };
     in {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
